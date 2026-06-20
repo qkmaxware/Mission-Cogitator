1e4bc5d44f9814769c7c8326097b42e24b40dce7
qkmaxware <qkmaxware@users.noreply.github.com>
2026-06-20 18:29:30 +0000

    Deploying to gh-pages from @ qkmaxware/Mission-Cogitator@79943ba2fb3a65acdea61720ac9199c54d6ad5f9 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "XnQfu/L1",
+  "version": "uLHQpOT+",
   "assets": [
     {
       "hash": "sha256-Nc9WGcQ/Y5+a5jon90XsZp9bS1OH9kyiT8f57GiDuuM=",
@@ -10,8 +10,8 @@ self.assetsManifest = {
       "url": "_framework/HtmlAgilityPack.st8ng737vz.wasm"
     },
     {
-      "hash": "sha256-FAEnvReaVTsAIZEfOnRRMAf22WzWiq5zpuTBg+cagQY=",
-      "url": "_framework/Kt.ofq1t63q7e.wasm"
+      "hash": "sha256-7oOfQ/XLOGaXCC+MU2HR2XkTqtaVj2APuAUjsLRVtiY=",
+      "url": "_framework/Kt.e1m20gm4s9.wasm"
     },
     {
       "hash": "sha256-GachBk5BHgrpaeO9o8QgqgUye80/dzuT/5XaYxdjYqA=",
@@ -210,16 +210,16 @@ self.assetsManifest = {
       "url": "_framework/blazor.webassembly.958z1vx7fr.js"
     },
     {
+      "hash": "sha256-JVElAYITUrlgzFKc/gF4qDcek6UxiS+govT2sq/jgzI=",
+      "url": "_framework/dotnet.54dbyqvp6t.js"
+    },
+    {
       "hash": "sha256-kkp5wX0htwkBcZt5WmEiKmhBqjqdCJtGc+koldfyoDQ=",
       "url": "_framework/dotnet.native.ikrs475e5v.js"
     },
     {
       "hash": "sha256-iQOJ2Ignl/X3n6mOHRQ4zWYcute0MGlaiRFi2J3HXWk=",
       "url": "_framework/dotnet.native.veuqw8a0w9.wasm"
-    },
-    {
-      "hash": "sha256-O6L2Dd9r/YgBp75QGxBGy27C+49tZ1Px6e6Ki6m2dbk=",
-      "url": "_framework/dotnet.rc78nxvtg9.js"
     },
     {
       "hash": "sha256-7i3usfTrnzC/9qWO4si5Bw4w7D9fUSnSBdhQ47blX2M=",
@@ -782,7 +782,7 @@ self.assetsManifest = {
       "url": "assets/packages/teams/battleclade/units/battleclade-technoarcheologist.html"
     },
     {
-      "hash": "sha256-cyG3dl0JHTJF6cOVi67yMjJb6o+d6bDl8RXKXlYYkzY=",
+      "hash": "sha256-mMUA82H651NtF+bYyimsmTMtDwrbhhq0ZLekALdTY4w=",
       "url": "assets/packages/teams/battleclade/units/battleclade-technomedic-servitor.html"
     },
     {
@@ -1522,7 +1522,7 @@ self.assetsManifest = {
       "url": "icon-192.png"
     },
     {
-      "hash": "sha256-rh32J8HDZE0LWnUdGcA1s4SRANeiC/eiwgfUj1YSJjM=",
+      "hash": "sha256-WeSDCCtQPyu2pw5350zxbJGlsg+95ZNurjFhCSxw2D8=",
       "url": "index.html"
     },
     {
